a783610c8721723a7e14e00f697f9ec6da68cb9f
Craycraft <Dan.Craycraft@lids.com>
2018-02-21 15:42:37 -0500

    g
    Signed-off-by: Craycraft <Dan.Craycraft@lids.com>

---
+++ fetchAndAsyncExample.js
@@ -0,0 +1,20 @@
+// function fetchAlumbs() {
+//     fetch('https://rallycoding.herokuapp.com/api/music_albums')
+//     .then( res => res.json() )
+//     .then(json => console.log(json))
+// }
+
+async function fetchAlbums() {
+    const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    const json = await res.json()
+    console.log(json)
+}
+fetchAlbums()
+
+/* OR DO IT THIS WAY */
+
+let fetchAlbums = async () => {
+    const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    const json = await res.json()
+    console.log(json)
+}
